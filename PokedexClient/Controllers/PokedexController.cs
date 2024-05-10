c8bd0ca7335ab1784b77b9f8caef56601aef58e7
Noriega29 <mateo2012ssj@hotmail.com>
2024-05-09 23:21:21 -0500

    Creación del Cliente
    Este cliente consumirá la Api Pokedex

---
+++ PokedexClient/Controllers/PokedexController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PokedexClient.Controllers
+{
+    public class PokedexController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public PokedexController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7249/api");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
