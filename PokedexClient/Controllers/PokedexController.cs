d1bf72636316e623afeb77e9b267243992b4d8fb
Noriega29 <mateo2012ssj@hotmail.com>
2024-05-11 14:57:47 -0500

    Creación base del cliente para la Api
    Por el momento en el cliente solo se puede hacer llamada a todos los los pokemon.

---
+++ PokedexClient/Controllers/PokedexController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PokedexClient.Models;
+
+namespace PokedexClient.Controllers
+{
+    public class PokedexController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public PokedexController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:44326/api/");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync("pokemons");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var pokemons = JsonConvert.DeserializeObject<IEnumerable<PokemonViewModel>>(content);
+                return View(pokemons);
+            }
+
+            return View(new List<PokemonViewModel>());
+        }
+    }
+}
