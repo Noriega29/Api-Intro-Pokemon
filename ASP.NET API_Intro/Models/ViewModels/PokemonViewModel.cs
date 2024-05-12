d94c9c91944ebe744158190bbf136b08f67b1bb9
Noriega29 <mateo2012ssj@hotmail.com>
2024-05-12 15:24:26 -0500

    Primeras correcciones a la Api-Server
    1. Cambié el nombre del proyecto de la Api de "ASP.NET API_Intro" a "PokedexServer"
    
    2. Borré todas las dependencias que no se estaban usando
    
    3. Ahora la propiedad Tipos de la clase PokemonViewModel es un Diccionario y no una Lista
    
    4. Se optimizaron los métodos necesarios en la clase PokemonViewModelService
    
    5. Por ultimo, ahora al hacer una consulta también se devuelve el nombre de los Tipos y no solo su ID en el cuerpo del JSON

--- ASP.NET API_Intro/Models/ViewModels/PokemonViewModel.cs
+++ ASP.NET API_Intro/Models/ViewModels/PokemonViewModel.cs
@@ -1,11 +1,13 @@
-﻿namespace ASP.NET_API_Intro.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NET_API_Intro.Models.ViewModels
 {
     public class PokemonViewModel
     {
         public int IdPokemon { get; set; }
         public int Numero { get; set; }
         public string Name { get; set; }
-        public List<int> Tipos { get; set; }
+        public Dictionary<int, string> Tipos { get; set; }
         public string Description { get; set; }
     }
 }
